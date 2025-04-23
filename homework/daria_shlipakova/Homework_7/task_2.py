11c261d734da79e36d972f909b5add5584496c04
Daria Shlipakova <dariashlipakova@gmail.com>
2025-04-23 17:48:10 +0300

    added blank lines after functions

--- homework/daria_shlipakova/Homework_7/task_2.py
+++ homework/daria_shlipakova/Homework_7/task_2.py
@@ -2,6 +2,7 @@ def repeating(dictionary):
     for word, number in dictionary.items():
         print(word * number)
 
+
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
 repeating(words)
--- homework/daria_shlipakova/Homework_7/task_3.py
+++ homework/daria_shlipakova/Homework_7/task_3.py
@@ -1,6 +1,7 @@
 def result(string):
     print(int(string.split()[-1]) + 10)
 
+
 result('результат операции: 42')
 result('результат операции: 54')
 result('результат работы программы: 209')
